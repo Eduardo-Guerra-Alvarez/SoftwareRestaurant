7acd12d6c4b4234e06b99bea4d3409a7e90a8d9a
Eduardo <lalo52rojo@gmail.com>
2025-04-06 17:18:56 -0600

    TableRestaurant CRUD created

---
+++ src/main/java/com/eduardo/softrestaurant/controller/TableRestaurantController.java
@@ -0,0 +1,46 @@
+package com.eduardo.softrestaurant.controller;
+
+import com.eduardo.softrestaurant.entity.TableRestaurant;
+import com.eduardo.softrestaurant.service.TableRestaurantService;
+import jakarta.persistence.Table;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/tables")
+public class TableRestaurantController {
+    @Autowired
+    private TableRestaurantService tableRestaurantService;
+
+    @GetMapping
+    public ResponseEntity<List<TableRestaurant>> getTables() {
+        return ResponseEntity.ok(tableRestaurantService.getAllTables());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TableRestaurant> getTable(@PathVariable Long id) {
+        return ResponseEntity.ok(tableRestaurantService.getTableById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TableRestaurant> createTable(@RequestBody TableRestaurant tableRestaurant) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tableRestaurantService.saveTable(tableRestaurant));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TableRestaurant> updateTable(@PathVariable Long id, @RequestBody TableRestaurant tableRestaurant) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(tableRestaurantService.updateTable(id, tableRestaurant));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteTable(@PathVariable Long id) {
+        return ResponseEntity.ok("Table Deleted");
+    }
+
+}
