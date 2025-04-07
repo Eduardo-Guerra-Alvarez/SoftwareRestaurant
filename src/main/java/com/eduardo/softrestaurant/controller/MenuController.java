9f6fb025462ae129381df10cbaa55ea52907c3ad
Eduardo <lalo52rojo@gmail.com>
2025-04-07 17:27:42 -0600

    created menu crud

---
+++ src/main/java/com/eduardo/softrestaurant/controller/MenuController.java
@@ -0,0 +1,44 @@
+package com.eduardo.softrestaurant.controller;
+
+import com.eduardo.softrestaurant.entity.Menu;
+import com.eduardo.softrestaurant.service.MenuService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/menu")
+public class MenuController {
+
+    @Autowired
+    private MenuService menuService;
+
+    @GetMapping
+    public ResponseEntity<List<Menu>> getMenus() {
+        return ResponseEntity.ok(menuService.getAllMenus());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Menu> getMenu(@PathVariable Long id) {
+        return ResponseEntity.ok(menuService.getMenu(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<Menu> createMenu(@RequestBody Menu menu) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(menuService.createMenu(menu));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Menu> updateMenu(@PathVariable Long id, @RequestBody Menu menu) {
+        return ResponseEntity.ok(menuService.updateMenu(id, menu));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteMenu(@PathVariable Long id) {
+        return ResponseEntity.ok("Menu deleted");
+    }
+}
