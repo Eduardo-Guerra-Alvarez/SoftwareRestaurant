4605ef740d62779be17aa93ac2f43abd6a0ce5b8
Eduardo-Guerra-Alvarez <lalo52rojo@gmail.com>
2025-07-10 19:05:50 -0600

    It was separated logic from backend with frontend

--- src/main/java/com/eduardo/softrestaurant/SoftrestaurantApplication.java
+++ src/main/java/com/eduardo/softrestaurant/SoftrestaurantApplication.java
@@ -1,7 +1,5 @@
 package com.eduardo.softrestaurant;
 
-import com.eduardo.softrestaurant.presentation.SoftwareRestaurantFX;
-import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,8 +8,8 @@ public class SoftrestaurantApplication {
 
 	public static void main(String[] args) {
 
-		//SpringApplication.run(SoftrestaurantApplication.class, args);
-		Application.launch(SoftwareRestaurantFX.class, args);
+		SpringApplication.run(SoftrestaurantApplication.class, args);
+		//Application.launch(SoftwareRestaurantFX.class, args);
 	}
 
 }
