5c65bfa1b2a56a5e1531f6f2999949050d62d9ab
Eduardo-Guerra-Alvarez <lalo52rojo@gmail.com>
2025-10-25 20:06:19 -0600

    implementation correctly of find by Table and by status

--- src/main/java/com/eduardo/softrestaurant/repository/OrderRepository.java
+++ src/main/java/com/eduardo/softrestaurant/repository/OrderRepository.java
@@ -1,5 +1,6 @@
 package com.eduardo.softrestaurant.repository;
 
+import com.eduardo.softrestaurant.OrderStatus;
 import com.eduardo.softrestaurant.dao.OrderDAO;
 import com.eduardo.softrestaurant.dao.OrderSimpleDAO;
 import com.eduardo.softrestaurant.entity.Order;
@@ -11,5 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-    List<OrderSimpleDAO> findByTableRestaurant_number(Long tableRestaurant_number);
+    List<Order> findByTableRestaurant_Id(Long tableId);
+    Optional<Order> findByTableRestaurant_IdAndStatus(Long tableRestaturant_number, OrderStatus status);
 }
